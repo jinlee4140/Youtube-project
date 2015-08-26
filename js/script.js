f22089d49708541a47f7a334e1abe5e769a66c65
jinlee4140 <jinlee4140@gmail.com>
2015-08-26 14:31:35 -0400

    javascript and jquery

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(function() {
+	var searchField = $('#query');     //search form
+	var icon = $('#search-btn');      // button
+
+	//Focus handler
+	$('searchField').on('focus', function() {
+		$(this).animate( {
+			width:'100%';
+		},400);
+		$(icon).animate( {
+			right: '10px';
+		},400);
+	});
+
+	// Blur Event Handler
+
+	$('searchField').on('focus', function() {
+		if(searchField.val() == '') {
+			$(searchField).animate({ 
+				width:'45%'
+			},400)
+		}
+	});
+
+})
